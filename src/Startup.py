41bb07f0e6fa58531624a663dfeb434ebdb3b69d
lennardclaproth <lennardclaproth@gmail.com>
2023-01-13 12:20:40 +0100

    changed startup

--- src/Startup.py
+++ src/Startup.py
@@ -7,7 +7,7 @@ from modules.logging.Logger import Logger
 from modules.user.context import UserContext
 from modules.blockchain.ChainHandler import ChainHandler
 from modules.view.RenderEngine import Loader as RenderEngine
-from modules.test.Server import Server
+from modules.server.Server import Server
 class Startup:
 
     @staticmethod
