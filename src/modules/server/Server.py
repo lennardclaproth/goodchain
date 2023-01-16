d338e1b68d022b309ee356c6fe63e2abbc25c05e
lennardclaproth <lennardclaproth@gmail.com>
2023-01-16 11:28:18 +0100

    test disconnect message

--- src/modules/server/Server.py
+++ src/modules/server/Server.py
@@ -82,7 +82,7 @@ class Server:
                         # client.send("hello".encode(self.FORMAT))
                             client.send(send_length)
                             client.send(message)
-                            client.send()
+                            client.send("!DISCONNECT")
                             connected = False
                         # client.send("!DISCONNECT".encode(self.FORMAT))
                     except OSError:
